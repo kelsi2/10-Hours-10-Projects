f707d8af89f330a00f3f1de2fb087ab0992c9819
Kelsi <kelsiproulx1919@gmail.com>
2020-11-11 12:21:53 -0800

    finished countdown timer

--- Countdown-Timer/script.js
+++ Countdown-Timer/script.js
@@ -1,3 +1,8 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
 const newYears = "1 Jan 2021";
 
 function countdown() {
@@ -9,6 +14,15 @@ function countdown() {
   const hours = Math.floor(totalSeconds / 3600) % 24;
   const minutes = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? (`0${time}`) : time;
 }
 
 // initial call
